09ab1075d485205695d1b9a2b1542bd49f545479
Lin Yun Wen <l40303k@gmail.com>
2019-01-26 19:13:40 +0800

    Feature: add data and init

---
+++ data.js
@@ -0,0 +1,62 @@
+export default {
+    news: {
+        parameters: {
+            controls: 0,
+            autoplay: 1,
+            fs: 1,
+            rel: 0
+        },
+        videos: [
+            {
+                number: 5,
+                title: "CNN 新聞",
+                videoId: "kW7FpFsBPiY"
+            },
+            {
+                number: 8,
+                title: "台視新聞",
+                videoId: "yk2CUjbyyQY"
+            },
+            {
+                number: 10,
+                title: "中視新聞",
+                videoId: "nmZUSI55CfA"
+            },
+            {
+                number: 12,
+                title: "華視新聞",
+                videoId: "TL8mmew3jb8"
+            },
+            {
+                number: 13,
+                title: "公共電視",
+                videoId: "_isseGKrquc"
+            },
+            {
+                number: 51,
+                title: "東森新聞",
+                videoId: "K_QgVWKW7TQ"
+            },
+            {
+                number: 52,
+                title: "中天新聞",
+                videoId: "wUPPkSANpyo"
+            },
+            {
+                number: 53,
+                title: "民視新聞",
+                videoId: "XxJKnDLYZz4"
+            },
+            {
+                number: 54,
+                title: "三立新聞",
+                videoId: "4ZVUmEUFwaY"
+            },
+            {
+                number: 55,
+                title: "TVBS 新聞",
+                videoId: "Hu1FkdAOws0"
+            }
+        ]
+    }
+}
